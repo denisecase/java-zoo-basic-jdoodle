43317fb09707de69936ee46420f12478cef8a290
Denise Case <denisecase@users.noreply.github.com>
2021-08-23 13:26:57 -0500

    Update Aardvark.java

--- Aardvark.java
+++ Aardvark.java
@@ -1,1 +1,13 @@
-
+public class Aardvark extends Animal {
+    
+    @Override
+    public void speak() {
+        System.out.println("I'm an Ardvark!");
+    }
+    
+    @Override
+    public void move() {
+        System.out.println("When I move, I walk, walk, walk.");
+    }
+    
+}
