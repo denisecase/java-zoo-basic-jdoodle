aa775e1a305065cb01f263f786e43a0c8daf5764
Denise Case <denisecase@users.noreply.github.com>
2021-08-23 13:26:20 -0500

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -1,1 +1,12 @@
-
+public class Animal {
+    
+    public void speak() {
+        System.out.println("Generic animal sound");
+    }
+    
+    public void move() {
+        System.out.println("Generic animal movement");
+    }
+    
+    
+}
