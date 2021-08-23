527f67f936fecf0521f40222ece5605e15d81f0f
Denise Case <denisecase@users.noreply.github.com>
2021-08-23 13:25:38 -0500

    Update ZooSimulation.java

--- ZooSimulation.java
+++ ZooSimulation.java
@@ -1,1 +1,17 @@
+public class ZooSimulation {
+    
+    static String greeting = "Welcome to Dr. Case's Zoo!";
+        
+    /*
+    * Execution begins here
+    */
+    public static void main(String args[]) {
 
+      System.out.println(greeting.toUpperCase());
+      
+      Aardvark alice = new Aardvark();
+      alice.speak();
+      alice.move();
+      
+    }
+}
